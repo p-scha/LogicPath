f3d7fa6eb2d75549aeb1721f44dcf3790225f658
p-scha <pjschachtv@gmail.com>
2026-02-10 16:09:56 -0500

    vite development server created for react, started start menu

---
+++ logic-path/src/components/StartMenu.jsx
@@ -0,0 +1,22 @@
+
+
+
+function StartMenu() {
+
+    return(
+        <>
+            <div className="start-menu">
+                <h1 className="title">LogicPath</h1>
+
+                <div className="menu">
+                    <button>Start</button> <br />
+                    <button>Options</button> <br />
+                    <button>Quit</button> <br />
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default StartMenu;
