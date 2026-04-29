20e8f13bf3778e34b414ff9187d96bf1fb0883c8
krisna <krishnapaneru31@gmail.com>
2026-04-28 21:55:00 -0400

    fix for login issue

--- logic-path/backend/src/main/java/com/logicpath/backend/LogicPathReposatory/UserRepository.java
+++ logic-path/backend/src/main/java/com/logicpath/backend/LogicPathReposatory/UserRepository.java
@@ -2,11 +2,9 @@ package com.logicpath.backend.LogicPathReposatory;
 
 import com.logicpath.backend.LogicPathModel.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
