8862a7c78fa16aba5fd1273f546c38eaf01cb20b
cande045 <cande045@odu.edu>
2026-04-29 20:08:43 -0400

    fixed issues with merge

--- logic-path/backend/src/main/java/com/logicpath/backend/BackendApplication.java
+++ logic-path/backend/src/main/java/com/logicpath/backend/BackendApplication.java
@@ -3,9 +3,11 @@ package com.logicpath.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.logicpath.backend.LogicPathModel")
+@EnableJpaRepositories(basePackages = "com.logicpath.backend.LogicPathReposatory")
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
